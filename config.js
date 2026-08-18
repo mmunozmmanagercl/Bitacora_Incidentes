26f080d85c38b49663fdac276dccb51731374563
mmunozmmanagercl <mmunozm@manager.cl>
2026-08-18 10:18:42 -0400

    Add files via upload

---
+++ config.js
@@ -0,0 +1,6 @@
+// ============================================================
+// Completa estos dos valores con los de tu proyecto Supabase.
+// Los encuentras en: Project Settings > API
+// ============================================================
+const SUPABASE_URL = "https://zvzidgimaecxcvipoiqc.supabase.co/rest/v1/";
+const SUPABASE_ANON_KEY = "zvzidgimaecxcvipoiqc";
